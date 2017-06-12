ccd65f82a290b85bbc9826f25919f297d2477c19
bruce <shiyj@5mutian.com>
2017-06-12 10:29:43 +0800

    修改配件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
+<style>
+
+
+
+</style>
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+
+<div id="app">
     <router-view></router-view>
-  </div>
+</div>
+
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default {
+    name: 'app'
 }
-</style>
+
+</script>
---
+++ src/components/home/home.vue
@@ -0,0 +1,26 @@
+<style lang="scss" scoped>
+
+
+
+</style>
+
+<template>
+
+<div id="home">
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: "home",
+    data: function data() {
+        return {
+
+        }
+    }
+}
+
+</script>
