5e1654eefef3c4f0727cc8d91954eac8451558ae
bruce <shiyj@5mutian.com>
2017-06-12 14:39:48 +0800

    添加登录等文件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/routers'
+import 'amazeui/dist/css/amazeui.min.css'
 
 Vue.config.productionTip = false
 
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -4,7 +4,6 @@
 <template>
 
 <div id="home">
-
 </div>
 
 </template>
