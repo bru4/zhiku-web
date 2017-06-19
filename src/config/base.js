5df6de82ff4cbdf2764ac3b858afa9c1f56dc372
bruce <shiyj@5mutian.com>
2017-06-19 14:23:21 +0800

    添加base设置文件

--- src/config/base.js
+++ src/config/base.js
@@ -1,9 +1,10 @@
 var baseUrl = ''
 
+// 本地跟线上的变量设置
 if (process.env.NODE_ENV == 'development') {
-    baseUrl = 'xxx'
+    baseUrl = ''
 }else if(process.env.NODE_ENV == 'production'){
-    baseUrl = 'xxxxxxxx'
+    baseUrl = ''
 }
 
 export { baseUrl }
