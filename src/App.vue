07be73fb5c8947db54693491c3270542a82b4dbe
bruce <shiyj@5mutian.com>
2017-06-20 17:27:03 +0800

    重置样式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <style>
-
 @import "./assets/styles/config.scss";
-@import "./assets/styles/app.scss";
 
 </style>
 
 <template>
-    <router-view></router-view>
+  <div class="app" id="app">
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <el-menu default-active="2" class="el-menu-vertical-demo" @open="" @close="">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>导航一</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
+            <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+          </el-menu>
+        </div>
+      </el-col>
+      <el-col :span="18">
+        <div class="grid-content bg-purple-light">
+          <router-view></router-view>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </div>
 
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import 'normalize.css/normalize.css'
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import router from './router/routers'
 import {baseUrl} from './config/base'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
