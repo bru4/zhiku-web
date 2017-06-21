e65419812cd76363b50e146c8b8c7c8567758992
bruce <shiyj@5mutian.com>
2017-06-21 12:04:55 +0800

    修改文件

--- src/pages/layout/layout.vue
+++ src/pages/layout/layout.vue
@@ -1,49 +1,56 @@
 <style lang="scss" scoped>
-@import "../../assets/styles/config.scss";
 
-.layout{
-  height: 100%;
-  .layout-left-and-right{
+@import "../../assets/styles/config.scss";
+.layout {
     height: 100%;
-    >.left{
-      position: fixed;
-      left: 0;
-      top: 0;
-      height: 100%;
+    .layout-left-and-right {
+        height: 100%;
+        > .left {
+            position: fixed;
+            left: 0;
+            top: 0;
+            height: 100%;
+        }
+        > .right {
+            padding-top: $layoutPadding;
+        }
     }
-    >.right{
-      padding-top: $layoutPadding;
-    }
-  }
 }
+
 </style>
 
 <template>
-  <div class="layout">
+
+<div class="layout">
     <head-bar></head-bar>
     <el-row class="layout-left-and-right">
-      <el-col :span="4" class="left">
-        <left-slider></left-slider>
-      </el-col>
-      <el-col :offset="4" :span="20" class="right">
-
-      </el-col>
+        <el-col :span="4" class="left">
+            <left-slider></left-slider>
+        </el-col>
+        <el-col :offset="4" :span="20" class="right">
+            <right-slider></right-slider>
+        </el-col>
     </el-row>
-  </div>
+</div>
+
 </template>
 
 <script>
+
 import HeadBar from './headBar.vue'
 import LeftSlider from './leftSlider.vue'
+import RightSlider from './rightSlider.vue'
+
 export default {
-  name: "layout",
-  components: {
-    HeadBar,
-    LeftSlider
-  },
-  data: function data() {
-    return {
+    name: "layout",
+    components: {
+        HeadBar,
+        LeftSlider,
+        RightSlider
+    },
+    data: function data() {
+        return {}
     }
-  }
 }
+
 </script>
---
+++ src/pages/layout/rightSlider.vue
@@ -0,0 +1,21 @@
+<style lang="scss" scoped>
+</style>
+
+<template>
+  <transition name="fade" mode="out-in">
+      <router-view :key="key"></router-view>
+  </transition>
+</template>
+
+<script>
+
+export default {
+    name: "",
+    data: function data() {
+        return {
+          key: ''
+        }
+    }
+}
+
+</script>
